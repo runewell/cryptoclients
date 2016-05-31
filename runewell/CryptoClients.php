5b5dd59b7d782feb525f2661fefd05e0aa568866
runewell <me@runewell.com>
2016-05-31 11:07:29 -0700

    testing class

--- runewell/CryptoClients.php
+++ runewell/CryptoClients.php
@@ -14,15 +14,16 @@ class CryptoClients
     /**
      * Create CryptoClients object
      *
-     * @param [type] $configArr [description]
+     * @param Array $configArr configuration array
      */
     public function __construct($configArr=[])
     {
         if (isset($configArr['ethereum']))
         {
+            $this->eth = 'testing ethereum';
             //$this->eth = new EthereumClient($configArr['ethereum']['node']);
         }
-        
+
         return $this;
     }
 
